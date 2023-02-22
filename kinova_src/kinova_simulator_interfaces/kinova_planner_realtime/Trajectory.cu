ccdeb887dcdf736dc3c18ad2c0b49e5301c51a3b
Cfather <jimzhang@umich.edu>
2023-02-22 16:31:23 -0500

    bug fix for k_dep term in qd_des and qdd_des

--- kinova_src/kinova_simulator_interfaces/kinova_planner_realtime/Trajectory.cu
+++ kinova_src/kinova_simulator_interfaces/kinova_planner_realtime/Trajectory.cu
@@ -144,8 +144,8 @@ void BezierCurve::makePolyZono(int s_ind) {
         // This is just a simplified implementation!!!
         // 30*t^2*(t - 1)^2 in qd_des is just a function with one maxima at t = 0.5
         // So as long as NUM_TIME_STEPS is even number, the following bounding trick holds!
-        k_dep_coeff_lb = 30 * pow(s_lb,2) * pow(s_lb - 1,2);
-        k_dep_coeff_ub = 30 * pow(s_ub,2) * pow(s_ub - 1,2);
+        k_dep_coeff_lb = 30 * pow(s_lb,2) * pow(s_lb - 1,2) / DURATION;
+        k_dep_coeff_ub = 30 * pow(s_ub,2) * pow(s_ub - 1,2) / DURATION;
         if (k_dep_coeff_ub < k_dep_coeff_lb) { // we are at t >= 0.5, which is a monotonically decreasing region 
             swap(k_dep_coeff_lb, k_dep_coeff_ub);
         }
@@ -188,22 +188,22 @@ void BezierCurve::makePolyZono(int s_ind) {
         qda_des(i, s_ind) = PZsparse(qd_des_center, qda_des_coeff, qda_des_degree, 2);
 
         // Part 3: qdd_des
-        double temp_lb = 60 * s_lb * (2 * pow(s_lb,2) - 3 * s_lb + 1);
-        double temp_ub = 60 * s_ub * (2 * pow(s_ub,2) - 3 * s_ub + 1);
+        double temp_lb = 60 * s_lb * (2 * pow(s_lb,2) - 3 * s_lb + 1) / DURATION / DURATION;
+        double temp_ub = 60 * s_ub * (2 * pow(s_ub,2) - 3 * s_ub + 1) / DURATION / DURATION;
         if (s_ub <= QDD_DES_K_DEP_MAXIMA) { // monotonically increasing region
             k_dep_coeff_lb = temp_lb;
             k_dep_coeff_ub = temp_ub;
         }
         else if (s_lb <= QDD_DES_K_DEP_MAXIMA) { // maxima lives inside
             k_dep_coeff_lb = min(temp_lb, temp_ub);
-            k_dep_coeff_ub = 60 * QDD_DES_K_DEP_MAXIMA * (2 * pow(QDD_DES_K_DEP_MAXIMA,2) - 3 * QDD_DES_K_DEP_MAXIMA + 1);
+            k_dep_coeff_ub = 60 * QDD_DES_K_DEP_MAXIMA * (2 * pow(QDD_DES_K_DEP_MAXIMA,2) - 3 * QDD_DES_K_DEP_MAXIMA + 1) / DURATION / DURATION;
         }
         else if (s_ub <= QDD_DES_K_DEP_MINIMA) { // monotonically decreasing region
             k_dep_coeff_lb = temp_ub;
             k_dep_coeff_ub = temp_lb;
         }
         else if (s_lb <= QDD_DES_K_DEP_MINIMA) { // minima lives inside
-            k_dep_coeff_lb = 60 * QDD_DES_K_DEP_MINIMA * (2 * pow(QDD_DES_K_DEP_MINIMA,2) - 3 * QDD_DES_K_DEP_MINIMA + 1);
+            k_dep_coeff_lb = 60 * QDD_DES_K_DEP_MINIMA * (2 * pow(QDD_DES_K_DEP_MINIMA,2) - 3 * QDD_DES_K_DEP_MINIMA + 1) / DURATION / DURATION;
             k_dep_coeff_ub = max(temp_lb, temp_ub);
         }
         else { // monotonically increasing region
