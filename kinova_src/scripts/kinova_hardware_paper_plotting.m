fc1a113e59ab4ea8dc6b5a5baf11e4e8dc5005e9
zacbrei <75452583+zacbrei@users.noreply.github.com>
2023-05-20 09:48:32 -0400

    Update kinova_hardware_paper_plotting.m

--- kinova_src/scripts/kinova_hardware_paper_plotting.m
+++ kinova_src/scripts/kinova_hardware_paper_plotting.m
@@ -66,8 +66,8 @@ surf_rad = 0.058/2;
 exp_time = data.rosbag.raw_time - data.rosbag.raw_time(1);
 
 % for nominal values
-pos = data.rosbag.debug_traj_pos;
-vel = data.rosbag.debug_traj_vel;
+pos = data.rosbag.debug_pos;
+vel = data.rosbag.debug_vel;
 % accel = data.rosbag.debug_traj_accel; % acceleration needs to be calculated
 opt_k = data.rosbag.debug_traj_k;
 
@@ -122,11 +122,11 @@ figure(fig_num)
 hold on
 
 subplot(3,1,1)
-plot(time,pos)
+plot(exp_time,pos)
 subplot(3,1,2)
-plot(time,vel)
+plot(exp_time,vel)
 subplot(3,1,3)
-plot(time,qdd_post)
+plot(exp_time,qdd_post)
 
 %%
 
