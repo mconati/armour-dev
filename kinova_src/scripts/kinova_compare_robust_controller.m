d97d7878c2b20e0cb5f73c48ccc972aea1f8e83a
Cfather <jimzhang@umich.edu>
2023-05-22 21:56:53 -0400

    latest script to analyze robust input

--- kinova_src/scripts/kinova_compare_robust_controller.m
+++ kinova_src/scripts/kinova_compare_robust_controller.m
@@ -15,11 +15,10 @@ input_constraints_flag = true;
 
 %%% for agent
 agent_urdf = 'kinova_without_gripper.urdf';
-
+model_uncertainties = [0, 0.03, 0.05, 0.1, 0.25, 0.5];
 add_uncertainty_to = 'all'; % choose 'all', 'link', or 'none'
 links_with_uncertainty = {}; % if add_uncertainty_to = 'link', specify links here.
-model_uncertainty = 0.50;
-uncertain_mass_range = [1 - model_uncertainty, 1 + model_uncertainty];
+
 
 agent_move_mode = 'integrator' ; % pick 'direct' or 'integrator'
 use_CAD_flag = false;
@@ -31,143 +30,149 @@ LLC_V_max = 1e-2;
 use_true_params_for_robust = false;
 if_use_mex_controller = true;
 
+for model_uncertainty = model_uncertainties
 %% robot params:
-robot = importrobot(agent_urdf);
-robot.DataFormat = 'col';
-robot.Gravity = [0 0 -9.81];
-params = load_robot_params(robot, ...
-                           'add_uncertainty_to', add_uncertainty_to, ...
-                           'links_with_uncertainty', links_with_uncertainty,...
-                           'uncertain_mass_range', uncertain_mass_range);
-joint_speed_limits = [-1.3963, -1.3963, -1.3963, -1.3963, -1.2218, -1.2218, -1.2218;
-                       1.3963,  1.3963,  1.3963,  1.3963,  1.2218,  1.2218,  1.2218]; % matlab doesn't import these from urdf so hard code into class
-joint_input_limits = [-56.7, -56.7, -56.7, -56.7, -29.4, -29.4, -29.4;
-                       56.7,  56.7,  56.7,  56.7,  29.4,  29.4,  29.4]; % matlab doesn't import these from urdf so hard code into class
-transmision_inertia = [8.02999999999999936 11.99620246153036440 9.00254278617515169 11.58064393167063599 8.46650409179141228 8.85370693737424297 8.85873036646853151]; % matlab doesn't import these from urdf so hard code into class
-M_min_eigenvalue = 5.095620491878957; % matlab doesn't import these from urdf so hard code into class
-eps = sqrt(2 * LLC_V_max / M_min_eigenvalue);
-
-% create arm agent
-A = uarmtd_agent(robot, params,...
-                 'verbose', verbosity,...
-                 'animation_set_axes_flag', 0,... 
-                 'animation_set_view_flag', 0,...
-                 'move_mode', agent_move_mode,...
-                 'use_CAD_flag', use_CAD_flag,...
-                 'joint_speed_limits', joint_speed_limits, ...
-                 'joint_input_limits', joint_input_limits, ...
-                 'add_measurement_noise_', add_measurement_noise_, ...
-                 'measurement_noise_size_', measurement_noise_size_,...
-                 'M_min_eigenvalue', M_min_eigenvalue, ...
-                 'transmision_inertia', transmision_inertia);
-
-%% automated from here
-robust_out_armour_final = zeros(A.n_inputs, 100);
-robust_out_althoff_final = zeros(A.n_inputs, 100);
-
-for sam = 1:100
-    fprintf('%d\n', sam);
-
-    %% initial conditions
-    q0 = pi * rand(A.n_inputs, 1) - pi / 2;
-    qd0 = pi * rand(A.n_inputs, 1) - pi / 2;
+    uncertain_mass_range = [1 - model_uncertainty, 1 + model_uncertainty];
     
-    z0 = zeros(A.n_states,1);
-    z0(A.joint_state_indices) = q0;
-    z0(A.joint_speed_indices) = qd0;
+    robot = importrobot(agent_urdf);
+    robot.DataFormat = 'col';
+    robot.Gravity = [0 0 -9.81];
+    params = load_robot_params(robot, ...
+                               'add_uncertainty_to', add_uncertainty_to, ...
+                               'links_with_uncertainty', links_with_uncertainty,...
+                               'uncertain_mass_range', uncertain_mass_range);
+    joint_speed_limits = [-1.3963, -1.3963, -1.3963, -1.3963, -1.2218, -1.2218, -1.2218;
+                           1.3963,  1.3963,  1.3963,  1.3963,  1.2218,  1.2218,  1.2218]; % matlab doesn't import these from urdf so hard code into class
+    joint_input_limits = [-56.7, -56.7, -56.7, -56.7, -29.4, -29.4, -29.4;
+                           56.7,  56.7,  56.7,  56.7,  29.4,  29.4,  29.4]; % matlab doesn't import these from urdf so hard code into class
+    transmision_inertia = [8.02999999999999936 11.99620246153036440 9.00254278617515169 11.58064393167063599 8.46650409179141228 8.85370693737424297 8.85873036646853151]; % matlab doesn't import these from urdf so hard code into class
+    M_min_eigenvalue = 5.095620491878957; % matlab doesn't import these from urdf so hard code into class
+    eps = sqrt(2 * LLC_V_max / M_min_eigenvalue);
     
-    %% trajectory params
-    traj_q0 = pi * rand(A.n_inputs, 1) - pi / 2;
-    traj_qd0 = pi * rand(A.n_inputs, 1) - pi / 2;
-    traj_qdd0 = pi * rand(A.n_inputs, 1) - pi / 2;
-    T = 2.5;
+    % create arm agent
+    A = uarmtd_agent(robot, params,...
+                     'verbose', verbosity,...
+                     'animation_set_axes_flag', 0,... 
+                     'animation_set_view_flag', 0,...
+                     'move_mode', agent_move_mode,...
+                     'use_CAD_flag', use_CAD_flag,...
+                     'joint_speed_limits', joint_speed_limits, ...
+                     'joint_input_limits', joint_input_limits, ...
+                     'add_measurement_noise_', add_measurement_noise_, ...
+                     'measurement_noise_size_', measurement_noise_size_,...
+                     'M_min_eigenvalue', M_min_eigenvalue, ...
+                     'transmision_inertia', transmision_inertia);
     
-    planner_info.desired_trajectory = {@(t)desired_trajectory(traj_q0, traj_qd0, traj_qdd0, t, T)};
+    %% automated from here
+    robust_out_armour_final = [];
+    robust_out_althoff_final = [];
     
-    %% run simulation amd process data (armour)
-    A.LLC = uarmtd_robust_CBF_LLC('verbose', verbosity, ...
-                                  'use_true_params_for_robust', use_true_params_for_robust, ...
-                                  'V_max', LLC_V_max, ...
-                                  'if_use_mex_controller', if_use_mex_controller);
+    for sam = 1:100
+        fprintf('%.2f %d\n', model_uncertainty, sam);
     
-    [tout_armour, zout_armour] = A.integrator(@(t,z) A.dynamics(t,z,planner_info), [0, T], z0);
+        %% initial conditions
+        q0 = pi * rand(A.n_inputs, 1) - pi / 2;
+        qd0 = pi * rand(A.n_inputs, 1) - pi / 2;
+        
+        z0 = zeros(A.n_states,1);
+        z0(A.joint_state_indices) = q0;
+        z0(A.joint_speed_indices) = qd0;
+        
+        %% trajectory params
+        q_diff = rand(A.n_inputs, 1) * 2 - 1; q_diff = pi * q_diff / norm(q_diff);
+        qd_diff = rand(A.n_inputs, 1) * 2 - 1; qd_diff = 2 * pi * qd_diff / norm(qd_diff);
+        traj_q0 = q0 + q_diff;
+        traj_qd0 = qd0 + qd_diff;
+        traj_qdd0 = zeros(A.n_inputs, 1);
+        T = 2.5;
+        
+        planner_info.desired_trajectory = {@(t)desired_trajectory(traj_q0, traj_qd0, traj_qdd0, t, T)};
+        
+        %% run simulation amd process data (armour)
+        A.LLC = uarmtd_robust_CBF_LLC('verbose', verbosity, ...
+                                      'use_true_params_for_robust', use_true_params_for_robust, ...
+                                      'V_max', LLC_V_max, ...
+                                      'if_use_mex_controller', if_use_mex_controller);
+        
+        [tout_armour, zout_armour] = A.integrator(@(t,z) A.dynamics(t,z,planner_info), [0, T], z0);
+        
+        q_armour = zout_armour(A.joint_state_indices,:);
+        qd_armour = zout_armour(A.joint_speed_indices,:);
+        
+        uout_armour = zeros(A.n_inputs, size(tout_armour, 2));
+        nominal_out_armour = zeros(size(uout_armour));
+        robust_out_armour = zeros(size(uout_armour));
+        
+        % store approximate inputs at each time:
+        for j = 1:length(tout_armour)
+            t = tout_armour(j);
+            z_meas = zout_armour(:,j);
+            [uout_armour(:, j), nominal_out_armour(:, j), robust_out_armour(:, j)] = A.LLC.get_control_inputs(A, t, z_meas, planner_info);
+        end
+        
+        robust_out_armour_final = [robust_out_armour_final, robust_out_armour];
     
-    q_armour = zout_armour(A.joint_state_indices,:);
-    qd_armour = zout_armour(A.joint_speed_indices,:);
+        %% run simulation amd process data (althoff)
+        A.LLC = uarmtd_robust_CBF_LLC('verbose', verbosity, ...
+                                      'use_true_params_for_robust', use_true_params_for_robust, ...
+                                      'V_max', LLC_V_max, ...
+                                      'if_use_mex_controller', if_use_mex_controller, ...
+                                      'if_use_armour_robust_controller', false);
+        
+        [tout_althoff, zout_althoff] = A.integrator(@(t,z) A.dynamics(t,z,planner_info), [0, T], z0);
+        
+        q_althoff = zout_althoff(A.joint_state_indices,:);
+        qd_althoff = zout_althoff(A.joint_speed_indices,:);
+        
+        uout_althoff = zeros(A.n_inputs, size(tout_armour, 2));
+        nominal_out_althoff = zeros(size(uout_armour));
+        robust_out_althoff = zeros(size(uout_armour));
+        
+        % store approximate inputs at each time:
+        for j = 1:length(tout_althoff)
+            t = tout_althoff(j);
+            z_meas = zout_althoff(:,j);
+            [uout_althoff(:, j), nominal_out_althoff(:, j), robust_out_althoff(:, j)] = A.LLC.get_control_inputs(A, t, z_meas, planner_info);
+        end
     
-    uout_armour = zeros(A.n_inputs, size(tout_armour, 2));
-    nominal_out_armour = zeros(size(uout_armour));
-    robust_out_armour = zeros(size(uout_armour));
-    
-    % store approximate inputs at each time:
-    for j = 1:length(tout_armour)
-        t = tout_armour(j);
-        z_meas = zout_armour(:,j);
-        [uout_armour(:, j), nominal_out_armour(:, j), robust_out_armour(:, j)] = A.LLC.get_control_inputs(A, t, z_meas, planner_info);
+        robust_out_althoff_final = [robust_out_althoff_final, robust_out_althoff];
     end
     
-    robust_out_armour_final = [robust_out_armour_final, robust_out_armour];
-
-    %% run simulation amd process data (althoff)
-    A.LLC = uarmtd_robust_CBF_LLC('verbose', verbosity, ...
-                                  'use_true_params_for_robust', use_true_params_for_robust, ...
-                                  'V_max', LLC_V_max, ...
-                                  'if_use_mex_controller', if_use_mex_controller, ...
-                                  'if_use_armour_robust_controller', false);
+    %% comparison
+    % [q_des, qd_des] = desired_trajectory(traj_q0, traj_qd0, traj_qdd0, tout_armour, T);
+    % 
+    % figure;
+    % for i = 1:7
+    %     subplot(3,3,i); hold on;
+    %     plot(tout_althoff, robust_out_althoff(i,:), 'r');
+    %     plot(tout_armour, robust_out_armour(i,:), 'b');
+    %     xlabel('time (sec)');
+    %     ylabel('robust input (N*m)');
+    %     if i == 1
+    %         legend('Althoff','Armour');
+    %     end
+    %     title(['joint', num2str(i)]);
+    % end
+    % sgtitle(['robust input (model uncertainty ', num2str(100 * model_uncertainty), ' %)']);
+    % 
+    % figure;
+    % for i = 1:7
+    %     subplot(3,3,i); hold on;
+    %     plot(tout_althoff, rad2deg(wrapToPi(q_althoff(i,:) - q_des(i,:))), 'r');
+    %     plot(tout_armour, rad2deg(wrapToPi(q_armour(i,:) - q_des(i,:))), 'b');
+    %     plot(tout_armour, -rad2deg(eps / A.LLC.Kr) * ones(1,length(tout_armour)), 'k');
+    %     plot(tout_armour, rad2deg(eps / A.LLC.Kr) * ones(1,length(tout_armour)), 'k');
+    %     xlabel('time (sec)');
+    %     ylabel('tracking error (degree)');
+    %     if i == 1
+    %         legend('Althoff','Armour','ultimate bound','ultimate bound');
+    %     end
+    %     title(['joint ', num2str(i)]);
+    % end
+    % sgtitle(['tracking error (model uncertainty ', num2str(100 * model_uncertainty), ' %)']);
     
-    [tout_althoff, zout_althoff] = A.integrator(@(t,z) A.dynamics(t,z,planner_info), [0, T], z0);
-    
-    q_althoff = zout_althoff(A.joint_state_indices,:);
-    qd_althoff = zout_althoff(A.joint_speed_indices,:);
-    
-    uout_althoff = zeros(A.n_inputs, size(tout_armour, 2));
-    nominal_out_althoff = zeros(size(uout_armour));
-    robust_out_althoff = zeros(size(uout_armour));
-    
-    % store approximate inputs at each time:
-    for j = 1:length(tout_althoff)
-        t = tout_althoff(j);
-        z_meas = zout_althoff(:,j);
-        [uout_althoff(:, j), nominal_out_althoff(:, j), robust_out_althoff(:, j)] = A.LLC.get_control_inputs(A, t, z_meas, planner_info);
-    end
-
-    robust_out_althoff_final = [robust_out_althoff_final, robust_out_althoff];
+    save(['robust_input_', num2str(100 * model_uncertainty), '.mat'], 'robust_out_armour_final', 'robust_out_althoff_final');
 end
-
-%% comparison
-% [q_des, qd_des] = desired_trajectory(traj_q0, traj_qd0, traj_qdd0, tout_armour, T);
-% 
-% figure;
-% for i = 1:7
-%     subplot(3,3,i); hold on;
-%     plot(tout_althoff, robust_out_althoff(i,:), 'r');
-%     plot(tout_armour, robust_out_armour(i,:), 'b');
-%     xlabel('time (sec)');
-%     ylabel('robust input (N*m)');
-%     if i == 1
-%         legend('Althoff','Armour');
-%     end
-%     title(['joint', num2str(i)]);
-% end
-% sgtitle(['robust input (model uncertainty ', num2str(100 * model_uncertainty), ' %)']);
-% 
-% figure;
-% for i = 1:7
-%     subplot(3,3,i); hold on;
-%     plot(tout_althoff, rad2deg(wrapToPi(q_althoff(i,:) - q_des(i,:))), 'r');
-%     plot(tout_armour, rad2deg(wrapToPi(q_armour(i,:) - q_des(i,:))), 'b');
-%     plot(tout_armour, -rad2deg(eps / A.LLC.Kr) * ones(1,length(tout_armour)), 'k');
-%     plot(tout_armour, rad2deg(eps / A.LLC.Kr) * ones(1,length(tout_armour)), 'k');
-%     xlabel('time (sec)');
-%     ylabel('tracking error (degree)');
-%     if i == 1
-%         legend('Althoff','Armour','ultimate bound','ultimate bound');
-%     end
-%     title(['joint ', num2str(i)]);
-% end
-% sgtitle(['tracking error (model uncertainty ', num2str(100 * model_uncertainty), ' %)']);
-
-save(['robust_input_', num2str(100 * model_uncertainty), '.mat'], 'robust_out_armour_final', 'robust_out_althoff_final');
 
 %% helper functions
 function [q_des, qd_des, qdd_des] = desired_trajectory(q0, qd0, qdd0, t, T)
--- kinova_src/scripts/kinova_compare_robust_controller_summary.m
+++ kinova_src/scripts/kinova_compare_robust_controller_summary.m
@@ -1,27 +1,51 @@
 close all; clear; clc;
 
-model_uncertainty = [0, 2.5, 5, 10, 25, 50];
+model_uncertainty = [0, 3, 5, 10, 25, 50];
 
-mean_robust_out_armour = zeros(7, length(model_uncertainty));
-mean_robust_out_althoff = zeros(7, length(model_uncertainty));
+x = 1:length(model_uncertainty);
+middle = zeros(7, 2, length(model_uncertainty));
+errhigh = zeros(7, 2, length(model_uncertainty));
+errlow = zeros(7, 2, length(model_uncertainty));  
 
 for i = 1:length(model_uncertainty)
     data = load(['robust_input_', num2str(model_uncertainty(i)), '.mat']);
-    mean_robust_out_armour(:,i) = data.mean_robust_out_armour_final;
-    mean_robust_out_althoff(:,i) = data.mean_robust_out_althoff_final;
+    middle(:,1,i) = median(abs(data.robust_out_armour_final), 2);
+    errhigh(:,1,i) = max(abs(data.robust_out_armour_final), [], 2) - middle(:,1,i);
+    errlow(:,1,i) = middle(:,1,i) - min(abs(data.robust_out_armour_final), [], 2);
+    middle(:,2,i) = median(abs(data.robust_out_althoff_final), 2);
+    errhigh(:,2,i) = max(abs(data.robust_out_althoff_final), [], 2) - middle(:,2,i);
+    errlow(:,2,i) = middle(:,2,i) - min(abs(data.robust_out_althoff_final), [], 2);
 end
 
+figure;
 for i = 1:7
-    subplot(3,3,i); hold on;
-    plot(model_uncertainty, mean_robust_out_althoff(i,:), 'r*-');
-    plot(model_uncertainty, mean_robust_out_armour(i,:), 'bo-');
-    if i == 1
-        legend('Althoff','Armour');
+    subplot(3,3,i);
+    hBar = bar(x,squeeze(middle(i,:,:)));   
+
+    hold on
+%     er = errorbar(x,squeeze(middle(i,:,:)),squeeze(errlow(i,:,:)),squeeze(errhigh(i,:,:)));    
+% %     er.Color = [0 0 0];                            
+%     er(1).LineStyle = 'none';
+%     er(2).LineStyle = 'none';
+    for j = 1:size(squeeze(middle(i,:,:)),1)
+        ctr(j,:) = bsxfun(@plus, hBar(j).XData, hBar(j).XOffset');   % Note: ‘XOffset’ Is An Undocumented Feature, This Selects The ‘bar’ Centres
+        ydt(j,:) = hBar(j).YData;                                     % Individual Bar Heights
     end
-    ylim([0, max([mean_robust_out_althoff(i,:), mean_robust_out_armour(i,:)])* 1.1]);
-    xticks(model_uncertainty);
-    xlabel('model uncertainty %');
-    ylabel('mean robust input (N*m)');
-    title(['joint ', num2str(i)]);
+    hold on
+    er = errorbar(ctr(1,:), ydt(1,:), squeeze(errlow(i,1,:)), squeeze(errhigh(i,1,:)));
+    er.LineStyle = 'none';
+    er.Color = [0, 0.4470, 0.7410];
+    er = errorbar(ctr(2,:), ydt(2,:), squeeze(errlow(i,2,:)), squeeze(errhigh(i,2,:)));
+    er.LineStyle = 'none';
+    er.Color = [0.8500, 0.3250, 0.0980];
+
+    ylabel('robust input (N*m)');
+    xlabel('model uncertainty');
+    set(gca,'XTickLabel',{'0%','3%','5%','10%','25%','50%'});
+
+    if i == 1
+        legend('armour', 'althoff');
+    end
 end
-sgtitle('robust input comparison');
+
+sgtitle('robust input comparison (median + min/max error bar)');
